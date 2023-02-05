ddb316a8a527339f9b5b6894216e284ee000ee97
andrehahn05 <andrehahn05@gmail.com>
2023-02-05 01:02:10 -0400

    activity form

---
+++ Notepad/app/src/main/java/com/hahn/notepad/ui/activity/NoteFormActivity.kt
@@ -0,0 +1,12 @@
+package com.hahn.notepad.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hahn.notepad.R
+
+class NoteFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_form)
+    }
+}
