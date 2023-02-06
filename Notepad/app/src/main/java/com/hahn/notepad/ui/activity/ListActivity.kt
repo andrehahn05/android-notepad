d34b4997d09d39d25c9ba2905c0ed6fd3afa089b
andrehahn05 <andrehahn05@gmail.com>
2023-02-06 16:17:19 -0400

    config fab

--- Notepad/app/src/main/java/com/hahn/notepad/ui/activity/ListActivity.kt
+++ Notepad/app/src/main/java/com/hahn/notepad/ui/activity/ListActivity.kt
@@ -1,16 +1,32 @@
 package com.hahn.notepad.ui.activity
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
-import com.hahn.notepad.R
+import androidx.appcompat.app.AppCompatActivity
 import com.hahn.notepad.databinding.ActivityListBinding
 
 class ListActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityListBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        handleFab()
+    }
+
+    private fun handleFab() {
+        val fab = binding.activityListFab
+        fab.setOnClickListener {
+            navigateToForm()
+        }
+    }
+
+    private fun navigateToForm() {
+        Intent(this@ListActivity, NoteFormActivity::class.java)
+            .apply {
+                startActivity(this)
+            }
     }
 }
